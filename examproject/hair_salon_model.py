7c8e3e5b4b3494fcc7db4eb99e4b9a4d54da58a8
Jasminsafai <lnp953@alumni.ku.dk>
2023-05-25 17:48:08 +0200

    prob 2.1

--- examproject/hair_salon_model.py
+++ examproject/hair_salon_model.py
@@ -1,22 +1,33 @@
-from scipy import optimize
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+class HairSalonModel:
+    def __init__(self, eta=0.5, w=1.0):
+        """
+        Initializes the HairSalonModel class with the given parameters.
+        
+        Parameters:
+        - eta: Elasticity of demand (Baseline: 0.5)
+        - w: Wage for each hairdresser (Baseline: 1.0)
+        """
+        self.eta = eta
+        self.w = w
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
-
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
-
-    """ 
-    
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
-
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+    def calculate_profit(self, kappa, ell=None):
+        """
+        Calculates the profit based on the given kappa and ell values.
+        
+        Parameters:
+        - kappa: Demand shock
+        - ell: Number of hairdressers producing haircuts (Baseline: None)
+        
+        Returns:
+        - profit: Calculated profit
+        """
+        if ell is None:
+            #If there is no value for ell, the code calculates it using the formula: ell = ((1 - eta) * kappa / w) ** (1 / eta)
+            #The calculate value for ell is then assigned to the "ell" variable, which is then used in the profit calculation
+            ell = ((1 - self.eta) * kappa / self.w) ** (1 / self.eta)
+        
+        #Calculating the profit using the formula: profit = kappa * ell ** (1 - eta) - w * ell
+        profit = kappa * ell ** (1 - self.eta) - self.w * ell
+        
+        return profit
